756869283de7a78aa3d046ccca335727d8aee2d9
Viswanath's personal laptop <viswanathkaliyamoorthy@gmail.com>
2024-01-02 22:10:08 +0530

    commit 2

--- python/test_ps10.py
+++ python/test_ps10.py
@@ -2,20 +2,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-
 
 @pytest.fixture
-def geturl(url):
+def logo():
+    driver = webdriver.Chrome()
     driver.maximize_window()
-    driver.get(url)
-
-
-@pytest.fixture()
-def getlogo(geturl("https://www.w3schools.com/")):
+    driver.get("https://www.w3schools.com/")
     logo = driver.find_element(By.ID, "w3-logo")
     return logo.is_diaplayed
 
 
-def test_w3s(getlogo):
-    assert getlogo == True
+def test_w3s(logo):
+    assert logo is True
